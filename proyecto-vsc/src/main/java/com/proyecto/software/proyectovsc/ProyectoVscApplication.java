0d75678944d84bb31e22821b117edc916ced6b58
JosheFlai1308 <jose.chavezr@sansano.usm.cl>
2024-03-29 14:46:34 -0300

    creacion proyecto

---
+++ proyecto-vsc/src/main/java/com/proyecto/software/proyectovsc/ProyectoVscApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.software.proyectovsc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoVscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoVscApplication.class, args);
+	}
+
+}
---
+++ proyecto-vsc/src/test/java/com/proyecto/software/proyectovsc/ProyectoVscApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.software.proyectovsc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoVscApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
